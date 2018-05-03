ea36b9f4e13f806dcf709bc80f48f91cd5ef7483
Saurav Sachidanand <sauravsachidanand@gmail.com>
2018-05-03 21:21:57 +0530

    Add Travis CI integration

--- tests/run.sh
+++ tests/run.sh
@@ -3,10 +3,14 @@
 TEST_DIR="$( cd "$( dirname "$0" )" && pwd )"
 SRC_DIR="$( dirname "$TEST_DIR" )"
 
-$SRC_DIR/emcc-cvmfs \
-    -o $TEST_DIR/test.html \
-    -s NO_EXIT_RUNTIME=0 \
-    --shell-file $TEST_DIR/test_container.html \
-    $TEST_DIR/test.c
+if [ "$1" != "--no-compile" ]; then
+    $SRC_DIR/emcc-cvmfs \
+        -o $TEST_DIR/test.html \
+        -s NO_EXIT_RUNTIME=0 \
+        --shell-file $TEST_DIR/test_container.html \
+        $TEST_DIR/test.c
+fi
 
-node $TEST_DIR/puppet.js $TEST_DIR/test.html
+if [ "$1" != "--no-test" ]; then
+    node $TEST_DIR/puppet.js $TEST_DIR/test.html
+fi
