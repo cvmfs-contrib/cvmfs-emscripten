31b97ff14a29bedc4ffc9844d56c3507a16deb1a
Saurav Sachidanand <sauravsachidanand@gmail.com>
2018-06-13 14:21:15 +0530

    Refactor readdir test and add support for printf in tests

--- tests/cpp/readdir.cpp
+++ tests/cpp/readdir.cpp
@@ -1,23 +1,23 @@
-#include <assert.h>
+#include <cassert>
+#include <string>
+#include <unordered_set>
 #include <dirent.h>
 #include <emscripten.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <string>
-#include <set>
 
 using namespace std;
 
-void check_entries(string dirpath, set<string>& entries) {
+void check_entries(const string& dirpath, const unordered_set<string>& entries) {
     DIR *dir;
     struct dirent *ent;
+    int found;
 
     dir = opendir(dirpath.c_str());
     assert(dir != NULL);
 
-    int found = 0;
+    found = 0;
     while ((ent = readdir(dir)) != NULL) {
       string entry = ent->d_name;
+
       if (entry == "." || entry == "..") continue;
 
       if (entries.find(entry) != entries.end())
@@ -32,15 +32,17 @@ int main() {
         window._cvmfs_testname = 'readdir';
     );
 
-    string testdir = "/cvmfs/emscripten.cvmfs.io/test";
-    set<string> testdir_entries = {
+    const string testdir = "/cvmfs/emscripten.cvmfs.io/test";
+    const unordered_set<string> testdir_entries = {
       "symlink", "varlink", "shake-128", "uncompressed",
       "chunked", "regular", "empty", "subdir", "nested"
     };
     check_entries(testdir, testdir_entries);
 
-    string subdir = "/cvmfs/emscripten.cvmfs.io/test/subdir";
-    set<string> subdir_entries = {"regular_subdir"};
+    const string subdir = "/cvmfs/emscripten.cvmfs.io/test/subdir";
+    const unordered_set<string> subdir_entries = {
+      "regular_subdir"
+    };
     check_entries(subdir, subdir_entries);
 
     return 0;
