cd08894100427172d244eb167af6b2ba9dd36c09
Marcin Mokrzan <mmokrzan@cern.ch>
2019-07-22 17:28:30 +0200

    Update cvmfs to main file

--- cvmfs/cvmfs.js
+++ cvmfs/cvmfs.js
@@ -1,7 +1,25 @@
-const cvmfs = {
-  retriever: {},
-  cache: {},
-  util: {},
+import { Repository, ENTRY_TYPE, CHUNK_HASH_ALG, COMPRESSION_ALG } from "./repo";
 
-  _master_keys: null
-};
+// TODO: Deprecated
+// const cvmfs = {
+//   retriever: {},
+//   cache: {},
+//   util: {},
+// };
+
+// TODO: Promise -> then chain
+const repository = new Repository('http://cvmfs-stratum-one.cern.ch/cvmfs', 'atlas.cern.ch');
+repository.connect().then(() => {
+    console.log('------------------------------------------------------------------');
+    console.log(repository.getManifest());
+    console.log(repository.getWhitelist());
+    console.log(repository.getCertificate());
+    console.log('------------------------------------------------------------------');
+
+    repository.getCatalog(manifest.catalogHash).then((catalog) => {
+        const statistics = repository.getCatalogStats(catalog);
+        console.log(statistics);
+    });
+});
+
+
