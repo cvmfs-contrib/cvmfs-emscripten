9ba5428d5b8f2cee34ef875e35500d40eb167f51
Marcin Mokrzan <mmokrzan@cern.ch>
2019-07-23 15:16:02 +0200

    Modified to export function

--- cvmfs/util.js
+++ cvmfs/util.js
@@ -7,59 +7,58 @@ export function repoURL(baseURL, repoName) {
   return baseURL + '/' + repoName;
 }
 
-cvmfs.util.dataURL = function(base_url, repo_name) {
-  return base_url + '/' + repo_name + '/data';
+export function dataURL(baseURL, repoName) {
+  return baseURL + '/' + repoName + '/data';
 }
 
-cvmfs.util.stringToHex = function(str) {
-  const len = str.length;
-  const hex = new Array(len);
-  for (var i = 0; i < len; i++) {
-    const byte = str.charCodeAt(i) & 0xff;
-    hex[i] = ('0' + byte.toString(16)).slice(-2);
+export function stringToHex(input) {
+  const buffer = Buffer.from(input, 'utf8');
+  return buffer.toString('hex');
+}
+
+export class Hash {
+  constructor(downloadHandle){
+    this.downloadHandle = downloadHandle;
+    const hashLength = downloadHandle.search('-');
+
+    if (hashLength === -1) {
+      this.hex = downloadHandle;
+      this.algorithm = 'sha1';
+    } else {
+      this.hex = downloadHandle.substring(0, hashLength);
+      this.algorithm = downloadHandle.substring(hashLength + 1);
+  
+      if (this.algorithm === 'rmd160') {
+        this.algorithm = 'ripemd160';
+      } 
+    }
   }
-  return hex.join('');
 }
 
-cvmfs.util.hash = function(download_handle) {
-  this.download_handle = download_handle;
-
-  const hash_len = download_handle.search('-');
-  if (hash_len === -1) {
-    this.hex = download_handle;
-    this.alg = 'sha1';
-  } else {
-    this.hex = download_handle.substring(0, hash_len);
-    this.alg = download_handle.substring(hash_len + 1);
-
-    if (this.alg === 'rmd160') this.alg = 'ripemd160';
+export function digestString(str, algorithm) {
+  if (algorithm === undefined) {
+    console.log('Error: No algorithm was specified for digestString');
+    return undefined;
   }
-}
-
-cvmfs.util.hash.prototype = {
-  hex: null,
-  alg: null,
-  download_handle: null
-};
-
-cvmfs.util.digestString = function(str, alg) {
-  if (alg === undefined) return undefined;
-  else if (alg === 'shake128') {
+  else if (algorithm === 'shake128') {
     const shake128 = new jsSHA("SHAKE128", "TEXT");
     shake128.update(str);
     return shake128.getHash("HEX", {shakeLen: 160});
   } else {
-    return rs.KJUR.crypto.Util.hashString(str, alg);
+    return crypto.Util.hashString(str, algorithm);
   }
 }
 
-cvmfs.util.digestHex = function(hex, alg) {
-  if (alg === undefined) return undefined;
-  else if (alg === 'shake128') {
+export function digestHex(hex, algorithm) {
+  if (algorithm === undefined) {
+    console.log('Error: No algorithm was specified for digestHex');
+    return undefined;
+  } 
+  else if (algorithm === 'shake128') {
     const shake128 = new jsSHA("SHAKE128", "HEX");
     shake128.update(hex);
     return shake128.getHash("HEX", {shakeLen: 160});
   } else {
-    return rs.KJUR.crypto.Util.hashHex(hex, alg);
+    return crypto.Util.hashHex(hex, algorithm);
   }
 }
