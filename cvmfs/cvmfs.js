312f94ba01c3c8b2413b9beb3e2ae7ecc01582a2
Marcin Mokrzan <mmokrzan@cern.ch>
2019-08-09 11:44:04 +0200

    Added manifest as variable

--- cvmfs/cvmfs.js
+++ cvmfs/cvmfs.js
@@ -9,13 +9,15 @@ import { Repository, ENTRY_TYPE, CHUNK_HASH_ALG, COMPRESSION_ALG } from "./repo"
 
 // TODO: Promise -> then chain
 const repository = new Repository('http://cvmfs-stratum-one.cern.ch/cvmfs', 'atlas.cern.ch');
+
 repository.connect().then(() => {
+    const manifest = repository.getManifest()
     console.log('------------------------------------------------------------------');
-    console.log(repository.getManifest());
+    console.log(manifest);
     console.log(repository.getWhitelist());
     console.log(repository.getCertificate());
     console.log('------------------------------------------------------------------');
-
+    
     repository.getCatalog(manifest.catalogHash).then((catalog) => {
         const statistics = repository.getCatalogStats(catalog);
         console.log(statistics);
