eab326af391c4ea1ce62de87d2e5ce21d53be6ab
Marcin Mokrzan <mmokrzan@cern.ch>
2019-08-13 10:49:33 +0200

    Checked straturm 0 revision

--- cvmfs/cvmfs.js
+++ cvmfs/cvmfs.js
@@ -1,4 +1,4 @@
-import { Repository, ENTRY_TYPE, CHUNK_HASH_ALG, COMPRESSION_ALG } from "./repo";
+import { Repository } from "./repo";
 
 // TODO: Deprecated
 // const cvmfs = {
@@ -8,16 +8,36 @@ import { Repository, ENTRY_TYPE, CHUNK_HASH_ALG, COMPRESSION_ALG } from "./repo"
 // };
 
 // TODO: Promise -> then chain
-const repository = new Repository('http://cvmfs-stratum-one.cern.ch/cvmfs', 'atlas.cern.ch');
+const repositoryWebsite = 'http://cvmfs-stratum-one.cern.ch/cvmfs';
+const repositoryName = 'atlas.cern.ch';
+const repository = new Repository(repositoryWebsite, repositoryName);
 
 repository.connect().then(() => {
-    const manifest = repository.getManifest()
+    const manifest = repository.getManifest();
+    const metainfo = repository.getMetainfo();
+    const metainfoJson = JSON.parse(metainfo)
+    const repositoryRevision = repository.getRevision();
+
     console.log('------------------------------------------------------------------');
     console.log(manifest);
     console.log(repository.getWhitelist());
     console.log(repository.getCertificate());
-    console.log(repository.getMetainfo());
-    console.log(repository.getRevision());
+    console.log(metainfoJson);
+    // console.log(metainfoJson['recommended-stratum0']);
+    // console.log(metainfoJson['recommended-stratum1s']);
+
+    for (const key of metainfoJson['recommended-stratum1s']){
+        const recommendedStratumOne = key.replace(`/${repositoryName}`, '').trim();
+        const stratumOneRepository = new Repository(recommendedStratumOne, repositoryName);
+            stratumOneRepository.connect().then(() => {
+                const revision = stratumOneRepository.getRevision();
+                if(repositoryRevision !== revision) {
+                    console.log(`!!!!Please update ${recommendedStratumOne}`);
+                } else{
+                    console.log(`Stratum 0 revision ${revision} is equal to Stratum 1 ${recommendedStratumOne} ${repositoryRevision}`);
+                }
+            });    
+    };
     console.log('------------------------------------------------------------------');
     
     // repository.getCatalog(manifest.catalogHash).then((catalog) => {
@@ -25,5 +45,3 @@ repository.connect().then(() => {
     //     console.log(statistics);
     // });
 });
-
-
