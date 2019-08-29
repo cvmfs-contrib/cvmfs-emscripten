80fd253f8a9a7ebe27217172b0b93f64913278c5
Marcin Mokrzan <mmokrzan@cern.ch>
2019-08-29 17:41:48 +0200

    Connection between frontend and backend

---
+++ cvmfs/server.js
@@ -0,0 +1,36 @@
+import { getJSONfromRpository } from './cvmfs';
+import express from 'express';
+
+const bodyParser = require('body-parser');
+
+const app = express();
+// for parsing application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const port = 5000;
+app.listen(port, () => `Server running on port ${port}`);
+
+app.get('/api/details', async (req, res) => {
+    let repositoryName = req.query.name;
+    let repositoryWebsite = req.query.website
+    // console.log("repositoryName", repositoryName);
+    // console.log("repositoryWebsite", repositoryWebsite);
+
+    try {
+        const reposonseJSON = await getJSONfromRpository(repositoryWebsite, repositoryName);
+        // console.log("reposonseJSON",reposonseJSON)
+        res.json(reposonseJSON);
+    } catch (error) {
+        console.log(error)
+        res.status(404).send('Not found');
+    }
+
+});
