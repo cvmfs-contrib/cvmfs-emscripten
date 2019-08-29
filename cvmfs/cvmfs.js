1bc136a37f5a61eec28bc57e9905613ff60ea55a
Marcin Mokrzan <mmokrzan@cern.ch>
2019-08-29 17:46:38 +0200

    Changed then to async

--- cvmfs/cvmfs.js
+++ cvmfs/cvmfs.js
@@ -1,37 +1,20 @@
 import { Repository } from "./repo";
-import express from 'express';
-// const fs = require('fs');
-const bodyParser = require('body-parser');
 
-const app = express();
-// for parsing application/json
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//enable CORS
- app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- });
 
 // TODO: Promise -> then chain
 // http://cvmfs-stratum-one.cern.ch/cvmfs/cms.cern.ch
-let repositoryWebsite = 'http://cvmfs-stratum-one.cern.ch/cvmfs';
-let repositoryName = 'atlas.cern.ch';
+// let repositoryWebsite = 'http://cvmfs-stratum-one.cern.ch/cvmfs';
+// let repositoryName = 'atlas.cern.ch';
 // const repositoryName = 'atlas-nightlies.cern.ch';
 // const repositoryName = 'alice-ocdb.cern.ch';
 
-app.post('/api/details', (req, res) => {
-    repositoryName = req.body.name;
-    repositoryWebsite = req.body.website
-    console.log("repositoryName", repositoryName);
-    console.log("repositoryWebsite", repositoryWebsite);
-});
 
-const repository = new Repository(repositoryWebsite, repositoryName);
 
-repository.connect().then(() => {
+export async function getJSONfromRpository(repositoryWebsite, repositoryName) {
+
+    let repository = new Repository(repositoryWebsite, repositoryName);
+    await repository.connect();
+
     const manifest = repository.getManifest();
     const metainfo = repository.getMetainfo();
     const whitelist = repository.getWhitelist();
@@ -57,105 +40,110 @@ repository.connect().then(() => {
     let stratumOneAllRevision = [];
     let hashAlgorithm = '';
 
-    for (const key of metainfoJson['recommended-stratum1s']){
+    for (const key of metainfoJson['recommended-stratum1s']) {
         // console.log("key", key);
         recommendedStratumOne = key.replace(`/${repositoryName}`, '').trim();
         const stratumOneRepository = new Repository(recommendedStratumOne, repositoryName)
-            // console.log("OUT recommendedStratumOne",recommendedStratumOne);
-            
-            stratumOneRepository.connect().then(() => {
-                // console.log("IN stratumOneRepository", stratumOneRepository);
-                // console.log("IN recommendedStratumOne",stratumOneRepository._baseURL);
-                revision = stratumOneRepository.getRevision();
-                publishedTimestamp = stratumOneRepository.getPublishedTimestamp();
-                // Assign state of repository
-                if(repositoryRevision === revision) {
-                    repositoryState = 'green';
-                } else if (repositoryRevision === revision + 1) {
-                    repositoryState = 'yellow';
-                } else if (repositoryRevision !== revision) {
-                    repositoryState = 'red';
-                };
-        
-                const metainfoForStratumOne = stratumOneRepository.getMetainfoForStratumOne();
-                const metainfoForStratumOneJson = JSON.parse(metainfoForStratumOne)
+        // console.log("OUT recommendedStratumOne",recommendedStratumOne);
 
-                stratumOne.push({
-                    url: stratumOneRepository._baseURL,
-                    revision: revision,
-                    health : repositoryState,
-                    id: i++,
-                    publishedTimestamp: publishedTimestamp,
-                    name: metainfoForStratumOneJson.organisation,
-                    location: ''
-                });
+        await stratumOneRepository.connect();
 
-                stratumOneAllRevision.push(revision);
+        // console.log("IN stratumOneRepository", stratumOneRepository);
+        // console.log("IN recommendedStratumOne",stratumOneRepository._baseURL);
+        revision = stratumOneRepository.getRevision();
+        publishedTimestamp = stratumOneRepository.getPublishedTimestamp();
+        // Assign state of repository
+        if (repositoryRevision === revision) {
+            repositoryState = 'green';
+        } else if (repositoryRevision === revision + 1) {
+            repositoryState = 'yellow';
+        } else if (repositoryRevision !== revision) {
+            repositoryState = 'red';
+        };
 
-                // Chceck revisoin for all stratumOne repositoris
-                if (stratumOne.find(x => x.health.includes('red'))) {
-                    healthStratumOne = 'red';
-                } else if (stratumOne.find(x => x.health.includes('yellow') && !x.health.includes('red'))) {
-                    healthStratumOne = 'yellow';
-                } else if (stratumOne.find(x => x.health.includes('green') && !x.health.includes('yellow') && !x.health.includes('red'))){
-                    healthStratumOne = 'green';
-                }
+        const metainfoForStratumOne = stratumOneRepository.getMetainfoForStratumOne();
+        const metainfoForStratumOneJson = JSON.parse(metainfoForStratumOne)
 
-                if(manifest.rootHash.includes("rmd160")){
-                    hashAlgorithm = "rmd160";
-                } else if (manifest.rootHash.includes("shake128")){
-                    hashAlgorithm = "shake128";
-                } else {
-                    hashAlgorithm = "SHA1";
-                }
-                
-            }).then(() => {
-                newJson.administrator = metainfoJson.administrator;
-                newJson.email = metainfoJson.email;
-                newJson.organisation = metainfoJson.organisation;
-                newJson.description = metainfoJson.description;
-                newJson.url = metainfoJson.url;
-                newJson.recommendedStratum0 = {
-                    url: metainfoJson['recommended-stratum0'],
-                    revision: repositoryRevision,
-                    publishedTimestamp:repositoryPublishedTimestamp
-                };
-                newJson.recommendedStratum1s = stratumOne;
-                newJson.custom = metainfoJson.custom;
-                newJson.health = healthStratumOne;
-                newJson.oldestRevisionStratumOne = Math.min(...stratumOneAllRevision);
-                newJson.whitelistExpiryDate = whitelist.expiryDate;
-                newJson.download = {
-                    catalog: '',
-                    certificate: '',
-                    manifest: '',
-                    whitelist: '',
-                };
-                newJson.rootHash = manifest.rootHash;
-                newJson.hashAlgorithm = hashAlgorithm;
+        stratumOne.push({
+            url: stratumOneRepository._baseURL,
+            revision: revision,
+            health: repositoryState,
+            id: i++,
+            publishedTimestamp: publishedTimestamp,
+            name: metainfoForStratumOneJson.organisation,
+            location: ''
+        });
 
-                console.log("stratumOneAllRevision", stratumOneAllRevision);
+        stratumOneAllRevision.push(revision);
 
-                let jsonStr = JSON.stringify(newJson);
+        // Chceck revisoin for all stratumOne repositoris
+        if (stratumOne.find(x => x.health.includes('red'))) {
+            healthStratumOne = 'red';
+        } else if (stratumOne.find(x => x.health.includes('yellow') && !x.health.includes('red'))) {
+            healthStratumOne = 'yellow';
+        } else if (stratumOne.find(x => x.health.includes('green') && !x.health.includes('yellow') && !x.health.includes('red'))) {
+            healthStratumOne = 'green';
+        }
 
-                // fs.writeFile('metainfofile.json', jsonStr, 'utf8', (err) => {
-                //     if(err) {
-                //         console.log(err);
-                //     };
-                //     console.log("File has been created");
-                // });
-                // console.log("newJson", newJson);
-            });        
+        if (manifest.rootHash.includes("rmd160")) {
+            hashAlgorithm = "rmd160";
+        } else if (manifest.rootHash.includes("shake128")) {
+            hashAlgorithm = "shake128";
+        } else {
+            hashAlgorithm = "SHA1";
+        }
+
+        newJson.administrator = metainfoJson.administrator;
+        newJson.email = metainfoJson.email;
+        newJson.organisation = metainfoJson.organisation;
+        newJson.description = metainfoJson.description;
+        newJson.url = metainfoJson.url;
+        newJson.recommendedStratum0 = {
+            url: metainfoJson['recommended-stratum0'],
+            revision: repositoryRevision,
+            publishedTimestamp: repositoryPublishedTimestamp
+        };
+        newJson.recommendedStratum1s = stratumOne;
+        newJson.custom = metainfoJson.custom;
+        newJson.health = healthStratumOne;
+        newJson.oldestRevisionStratumOne = Math.min(...stratumOneAllRevision);
+        newJson.whitelistExpiryDate = whitelist.expiryDate;
+        newJson.download = {
+            catalog: '',
+            certificate: '',
+            manifest: '',
+            whitelist: '',
+        };
+        // newJson.rootHash = manifest.rootHash;
+        // newJson.hashAlgorithm = hashAlgorithm;
+
+        newJson.rootHash = manifest.catalogHash.downloadHandle;
+        newJson.hashAlgorithm = manifest.catalogHash.algorithm;
+
+        // console.log("stratumOneAllRevision", stratumOneAllRevision);
+
+        // let jsonStr = JSON.stringify(newJson);
+
+        // fs.writeFile('metainfofile.json', jsonStr, 'utf8', (err) => {
+        //     if(err) {
+        //         console.log(err);
+        //     };
+        //     console.log("File has been created");
+        // });
+        // console.log("newJson", newJson); 
     };
 
-    app.get('/api', (req, res) => {
-        const repositoryJson =  newJson;
+    // console.log("newJson", newJson);
+    return newJson;
 
-        res.json(repositoryJson);
-      });
+    // app.get('/api', (req, res) => {
+    //     const repositoryJson =  newJson;
 
-      const port = 5000;
-      
-      app.listen(port, () => `Server running on port ${port}`);
-    console.log('------------------------------------------------------------------');
-});
+    //     res.json(repositoryJson);
+    // });
+
+    // const port = 5000;
+
+    // app.listen(port, () => `Server running on port ${port}`);
+    // console.log('------------------------------------------------------------------');
+}
