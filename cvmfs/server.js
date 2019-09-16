770f1c88c1741fbfec09652f63d0d2a44df9e6e5
Marcin Mokrzan <mmokrzan@cern.ch>
2019-09-16 16:21:31 +0200

    Updated cors

--- cvmfs/server.js
+++ cvmfs/server.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //enable CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods","GET")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Controle");
     next();
 });
 
